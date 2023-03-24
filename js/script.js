3ecbf8dee654213affea4efbea57bcc4c2840b9b
Gustavo Toná <gustavopicolitona@gmail.com>
2023-03-24 00:08:10 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.getElementById("calculate").addEventListener("click", function(){
+
+let value = document.getElementById("value").value;
+let fee = (document.getElementById("fee").value) / 100;
+let time = document.getElementById("time").value;
+
+const total = value * (
+    1 + fee) **time;
+
+    document.getElementById("total").innerHTML = ("R$" + total.toFixed(2));
+
+
+});
